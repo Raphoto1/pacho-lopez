cf6bb2f868ae5070fad9fbbf9a92e6200bd7a22e
¨Rafael <rrhhmmtt@gmail.com>
2025-10-31 11:55:19 -0500

    ajuste en footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className='flex flex-col w-full h-auto bg-gray-800/50 justify-around items-center py-4 mt-auto'>
-      <div className="flex w-full h-auto justify-around items-center py-4 mt-auto">
+      <div className="flex flex-col md:flex-row w-full h-auto justify-around items-center py-4 mt-auto">
         <div className="flex flex-col justify-center align-middle items-center">
           <h3 className='font-bold text-center md:text-2xl md:text-start'>Pacho López y la Cumbia Mestiza 2025</h3>
           <p className='text-sm text-center md:text-start'>Todos los derechos reservados</p>
