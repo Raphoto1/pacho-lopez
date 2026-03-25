8b8a689c1337c52cecf9c22caddc1508c89c4bc5
¨Rafael <rrhhmmtt@gmail.com>
2026-03-25 14:20:36 -0500

    se aggregan fotos y proximos shows al hoome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-
 import VideoGrid from "@/components/grid/VideoGrid";
 import SocialBar from "@/components/bar/SocialBar";
 import HeroVideo from "@/components/header/HeroVideo";
@@ -6,7 +5,15 @@ import CarouselHalf2 from "@/components/carousel/CarouselHalf2";
 import CarouselBanners from "@/components/carousel/CarouselBanners";
 import MusicPlayer from "@/components/musicPlayer/MusicPlayer";
 import MusicPlayerDisc from "@/components/musicPlayer/MusicPlayerDisc";
+import ToursGrid from "@/components/tour/ToursGrid";
+import CarouselPhotos from "@/components/carousel/CarouselPhotos";
 export default function Home() {
+  // Generate array for all 52 photos in carousel3
+  const carouselImages = Array.from({ length: 52 }, (_, i) => ({
+    id: i,
+    src: `/img/photos/carousel3/ph${i}.JPG`,
+    alt: `Pacho López Live - Photo ${i + 1}`,
+  }));
 
   return (
     <div>
@@ -15,8 +22,10 @@ export default function Home() {
       <MusicPlayerDisc />
       <CarouselBanners />
       <MusicPlayer />
-      <CarouselHalf2 />
+      {/* <CarouselHalf2 /> */}
       <VideoGrid />
+      <ToursGrid />
+      <CarouselPhotos carouselImages={carouselImages} />
     </div>
   );
 }
